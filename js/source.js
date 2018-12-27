e464b52c81e8cf050dc38e9088a608b8e04afadc
ginNyang2 <bbsnake10@gmail.com>
2018-12-27 17:45:30 +0900

    [Modify]1. annotaion about entry[0] and [1] of source.checkEmptyInput(). 2. get rid of last rest symbol and space from alert message of source.checkEmptyInput(). 3. annotation on createTextNode vs innerHTML at source.createSentMailNode(). 4. define values of start and end index of source.clearInput() with annotation.

--- js/source.js
+++ js/source.js
@@ -11,15 +11,20 @@ const source = { //namespace 활용. 외부 파일에서 접근시 어느 파일
     let emptyEntries = [];
     entries.forEach((entry) => {
       if(!entry[1]) emptyEntries.push(entry[0]);
+      /* entries = [["key1", "value1"], ["key2", "value2"], ... ]
+         entry[0]: key: name attribute of form <input>s which are "category", "destination", "title", "comment"
+         entry[1]: value: user input value of "category", "destination", "title", "comment"
+      */
     });
 
     if(emptyEntries.length) {
-      const message = emptyEntries.reduce((accum, value) => {
-        if(value === 'destination') value = '받는사람';
+      let message = emptyEntries.reduce((accum, value) => {
+        if(value === 'destination') value = '받는 사람';
         else if(value === 'title') value = '제목';
         else value = '내용';
         return accum + value + ', ';
       }, '');
+      message = message.slice(0,-2);//get rid of ', ' which is last character and space of alert message
       alert(`${message}을(를) 입력해주세요.`);
       return false;
     }
@@ -38,7 +43,8 @@ const source = { //namespace 활용. 외부 파일에서 접근시 어느 파일
 
     const result = `${year}년 ${month}월 ${date}일 ${hours}시 ${minutes}분 ${seconds}초`;
 
-    assert(result.indexOf('undefined')===-1, 'some Date value is undefined');
+    //confirm whether get---() methods of Date function operate correctly.
+    assert(result.indexOf('undefined') === -1, 'some Date value is undefined');
     return result;
   },
 
@@ -48,7 +54,7 @@ const source = { //namespace 활용. 외부 파일에서 접근시 어느 파일
 
     let extractedData = {};
     for (let entry of entriesArray) {
-        extractedData[entry[0]] = entry[1];
+        extractedData[entry[0]] = entry[1];// entry[0]: name attribute of form <input>s, entry[1]: user input
     }
     extractedData["time"] = currentTime;
     console.log(extractedData);
@@ -62,9 +68,9 @@ const source = { //namespace 활용. 외부 파일에서 접근시 어느 파일
 
     //create node
     const sentMailNode = document.createElement("div");
-    const paragraphNode = document.createElement("P");
+    const paragraphNode = document.createElement("p");
 
-    //create textnode
+    //createTextnode vs innerHTML: innerHTML is a 'html like' string. It would be better to use createTextNode if you want to supervise user input as childNodes.
     const category = document.createTextNode(`분류: ${data.category} `);
     const destination = document.createTextNode(`받는사람: ${data.destination} `);
     const title = document.createTextNode(`제목: ${data.title} `);
@@ -83,10 +89,13 @@ const source = { //namespace 활용. 외부 파일에서 접근시 어느 파일
     return sentMailNode;
   },
 
-  clearInput : (target) => {
-    assert(target, 'target paramter is not defined');
-    for(let i = 3; i < 6; i++){
-      target[i].value='';
+  clearInput : (targets) => {
+    assert(targets, 'target paramter is not defined');
+    const indexOfDestination = 3;
+    const indexOfComment = 5;//4 is index of title
+    //index 0, 1, 2 values of targets are <input> named "category" which are "동료", "친구", and "가족"
+    for(let i = indexOfDestination; i <= indexOfComment; i++) {
+      targets[i].value = '';
     }
   }
 
